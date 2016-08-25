727051665a247d1a2001e8ac450bc6b89e760c73
ame <ameeta.viegas@gmail.com>
2016-08-25 16:44:51 -0400

    resolved regex for phone #

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,20 +2,16 @@ class Customer < ApplicationRecord
   has_many :call_logs
   has_many :appointments
 
-  # validations
-  # validates_format_of :home_phone,
-  #                     with: /\(\d{3}\) ?\d{3}( |-)?\d{4}|^\d{3}( |-)?\d{3}( |-)?\d{4}/i,
-  #                     :allow_blank => true
 
   validates_format_of :home_phone,
-                      with: /\(\d{3}\) ?\d{3}( |-)?\d{4}|^\d{3}( |-)?\d{3}( |-)?\d{4}/i,
+                      with: /\A(\(?(\d{3})\)?[-. ]\d{3}[-.]\d{4})\z/i,
                       allow_blank:  true,
-                      message: "Please correct home number"
+                      message: "Please correct home number. Format: ###-###-####"
 
   validates_format_of :cell_phone,
-                      with: /\(\d{3}\) ?\d{3}( |-)?\d{4}|^\d{3}( |-)?\d{3}( |-)?\d{4}/i,
+                      with: /\A(\(?(\d{3})\)?[-. ]\d{3}[-.]\d{4})\z/i,
                       :allow_blank => true,
-                      message: "Please correct cell number"
+                      message: "Please correct cell number. Format: ###-###-####"
 
 
   validates :name,
